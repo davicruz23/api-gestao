9cd40cb62077c38482038c83348f977345af3de1
Davi Cruz <davifieledeus@gmail.com>
2025-11-07 22:12:35 -0300

    ajuste pro render host!

--- src/main/java/tads/ufrn/apigestao/config/WebConfig.java
+++ src/main/java/tads/ufrn/apigestao/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Permite todas as rotas que começam com /api/
-                .allowedOrigins("https://api-gestao-n29u.onrender.com") // Permite origem do frontend
+                .allowedOrigins("http://192.168.10.5:5173") // Permite origem do frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite credenciais
